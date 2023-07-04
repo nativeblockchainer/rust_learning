c2378f53c30fb9f65fd835d54b6bd96b0911b0f1
yeshun <yeshun@tdft.cn>
2023-07-04 09:41:05 +0800

    修改层级

--- course_1/src/helper/print_ascii.rs
+++ course_1/src/helper/print_ascii.rs
@@ -20,22 +20,3 @@ pub fn print_ascii_by_while(start: char,end: char){
         }
     }
 }
-    
-/**
- * 用for循环实现
- */
-pub fn print_ascii_by_for(start: char,end: char){
-    let start_dec: u8 = start as u8;
-    let end_dec: u8 = end as u8;
-    println!("打印{}({})到{}({})之间的字符！",start,start_dec,end,end_dec);
-    
-    if start_dec >= end_dec {
-        for c in (end..=start).rev() {
-            println!("{}",c);
-        }
-    } else {
-        for c in start..=end {
-            println!("{}",c);
-        }
-    }
-}
--- course_1/src/main.rs
+++ course_1/src/main.rs
@@ -1,12 +1,12 @@
 mod helper;
 fn main() {
-    // 大于'a'到'Z'之间的字符
+    // 一层module，打印'a'到'Z'之间的字符
+    helper::print_ascii_by_for('a','Z');
+    //  一层module，打印'A'到'z'之间的字符
+    helper::print_ascii_by_for('A','z');
+
+    // 二层module，打印'a'到'Z'之间的字符
     helper::print_ascii::print_ascii_by_while('a','Z');
-    // 大于'A'到'z'之间的字符
+    // 二层module，打印'A'到'z'之间的字符
     helper::print_ascii::print_ascii_by_while('A','z');
-    
-    // 大于'a'到'Z'之间的字符
-    helper::print_ascii::print_ascii_by_for('a','Z');
-    // 大于'A'到'z'之间的字符
-    helper::print_ascii::print_ascii_by_for('A','z');
 }
